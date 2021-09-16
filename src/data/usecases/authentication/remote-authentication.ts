209529812ea7338030a9e7682e61dceb4fd7c5c4
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-16 11:35:20 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,13 @@
+import { HttpPostClient } from '../../protocols/http/http-post-client';
+
+export class RemoteAuthentication {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {} // eslint-disable-line no-empty-function
+
+  async auth(): Promise<void> {
+    await this.httpPostClient.post(this.url);
+    return Promise.resolve();
+  }
+}
