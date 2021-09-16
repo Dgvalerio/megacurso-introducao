6dca9ca3393c618d646ca934decfec8808632d80
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-16 16:50:51 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,6 @@
 import { InvalidCredentialsError } from '../../../domain/errors/invalid-credentials-error';
 import { UnexpectedError } from '../../../domain/errors/unexpected-error';
+import { AccountModel } from '../../../domain/models/account-model';
 import { AuthenticationParams } from '../../../domain/usecases/authentication';
 import { HttpPostClient } from '../../protocols/http/http-post-client';
 import { HttpStatusCode } from '../../protocols/http/http-response';
@@ -7,7 +8,10 @@ import { HttpStatusCode } from '../../protocols/http/http-response';
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
+    private readonly httpPostClient: HttpPostClient<
+      AuthenticationParams,
+      AccountModel
+    >
   ) {} // eslint-disable-line no-empty-function
 
   async auth(params: AuthenticationParams): Promise<void> {
@@ -15,6 +19,7 @@ export class RemoteAuthentication {
       url: this.url,
       body: params,
     });
+
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         break;
