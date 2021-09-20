c4edbc1d90f3f82560ac8d2e564ce5425b8efeb6
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-20 15:19:13 -0300

    test: ensure Login present valid state if "password" is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -85,4 +85,22 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
+
+  test('should show valid password state if Validation succeeds', () => {
+    const {
+      sut: { getByTestId },
+      validationStub,
+    } = makeSut();
+    validationStub.errorMessage = null;
+
+    const passwordInput = getByTestId('password');
+    const passwordStatus = getByTestId('password-status');
+
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+
+    expect(passwordStatus.title).toBe('Tudo certo!');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
