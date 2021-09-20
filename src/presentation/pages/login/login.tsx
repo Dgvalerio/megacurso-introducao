5f3cd262d38ca5c66c57497b225ce2e22ad754fd
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-20 09:02:01 -0300

    feat: ensure Login will not render spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,33 +1,48 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { Footer, FormStatus, Input, LoginHeader } from '../../components';
+import Context from '../../contexts/form/form-context';
 import Styles from './login-styles.scss';
 
-const Login: FC = () => (
-  <div className={Styles.login}>
-    <LoginHeader />
-    <form className={Styles.form}>
-      <h2>Login</h2>
-      <Input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Digite seu e-mail"
-      />
-      <Input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Digite seu senha"
-      />
-      <button className={Styles.submit} type="submit">
-        Entrar
-      </button>
-      <span className={Styles.link}>Criar conta</span>
-      <FormStatus />
-    </form>
-    <Footer />
-  </div>
-);
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
+
+const Login: FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
+  return (
+    <div className={Styles.login}>
+      <LoginHeader />
+      <Context.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite seu senha"
+          />
+          <button className={Styles.submit} type="submit">
+            Entrar
+          </button>
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
 
 export default Login;
