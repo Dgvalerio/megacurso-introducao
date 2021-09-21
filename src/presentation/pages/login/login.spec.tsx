56f9a9d3558a3821714be22b4d30e09a57ac9f8b
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-21 09:48:13 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -155,4 +155,13 @@ describe('Login Component', () => {
 
     expect(authenticationSpy.params).toEqual({ email, password });
   });
+
+  test('should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+
+    simulateValidSubmit(sut);
+    simulateValidSubmit(sut);
+
+    expect(authenticationSpy.callsCount).toBe(1);
+  });
 });
