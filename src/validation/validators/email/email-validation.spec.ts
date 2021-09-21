f2b0b0f1943d929f427a97e8c92f16cc25868e6b
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-21 11:52:15 -0300

    test: ensure Email validation return error if email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '../../errors';
+import { EmailValidation } from './email-validation';
+
+describe('EmailValidation', () => {
+  test('should return error if email is invalid', () => {
+    const sut = new EmailValidation('email');
+
+    const error = sut.validate('');
+
+    expect(error).toEqual(new InvalidFieldError('email'));
+  });
+});
