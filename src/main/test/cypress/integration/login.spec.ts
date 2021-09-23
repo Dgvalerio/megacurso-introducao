8aebc74f4213a0d7e8c6362e3a6f4db4a35ff95c
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-23 20:42:31 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,15 +1,39 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
   });
 
   it('should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly');
+
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴');
+
+    cy.getByTestId('password').should('have.attr', 'readOnly');
+
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word());
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'O campo email tem um valor inválido')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(2));
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'O campo password tem um valor inválido')
+      .should('contain.text', '🔴');
+
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
