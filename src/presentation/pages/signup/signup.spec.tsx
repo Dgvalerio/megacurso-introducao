6d67da24d581d086cfdd32536d1413f871bbb1f4
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-23 09:12:34 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -35,7 +35,7 @@ describe('SignUp Component', () => {
     Helper.testButtonIsDisabled(sut, 'submit', true);
 
     Helper.testStatusForField(sut, 'name', validationError);
-    Helper.testStatusForField(sut, 'email', 'Campo obrigatório');
+    Helper.testStatusForField(sut, 'email', validationError);
     Helper.testStatusForField(sut, 'password', 'Campo obrigatório');
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório');
   });
@@ -47,5 +47,14 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'name');
 
     Helper.testStatusForField(sut, 'name', validationError);
+  });
+
+  test('should show name error if Validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+
+    Helper.populateField(sut, 'email');
+
+    Helper.testStatusForField(sut, 'email', validationError);
   });
 });
