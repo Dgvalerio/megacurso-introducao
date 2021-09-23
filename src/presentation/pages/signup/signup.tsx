e50397c4665c406294b3b3c7b88e8ff6921a2c1b
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-22 23:15:38 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,51 +1,64 @@
-import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import React, { FC, useState } from 'react';
 
 import { Footer, FormStatus, Input, LoginHeader } from '../../components';
 import Context from '../../contexts/form/form-context';
 import Styles from './signup-styles.scss';
 
-const SignUp: FC = () => (
-  <div className={Styles.signup}>
-    <LoginHeader />
-    <Context.Provider value={{ state: {} }}>
-      <form className={Styles.form}>
-        <h2>Criar Conta</h2>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Digite seu nome"
-        />
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Digite seu e-mail"
-        />
-        <Input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Digite sua senha"
-        />
-        <Input
-          type="password"
-          id="passwordConfirmation"
-          name="passwordConfirmation"
-          placeholder="Repita sua senha"
-        />
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
-        <Link className={Styles.link} to="/login">
-          Voltar para Login
-        </Link>
-        <FormStatus />
-      </form>
-    </Context.Provider>
-    <Footer />
-  </div>
-);
+const SignUp: FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: '',
+  });
+
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+      <Context.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+          />
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <Input
+            type="password"
+            id="passwordConfirmation"
+            name="passwordConfirmation"
+            placeholder="Repita sua senha"
+          />
+          <button
+            className={Styles.submit}
+            type="submit"
+            data-testid="submit"
+            disabled
+          >
+            Entrar
+          </button>
+          <span className={Styles.link}>Voltar para Login</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
 
 export default SignUp;
