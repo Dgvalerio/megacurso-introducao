f07ba9815a7f171a19a37d41b6769a722960b0e0
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-27 20:48:52 -0300

    feat: ensure RemoteLoadSurveyList calls HttpGetClient with correct URL

--- src/data/protocols/http/index.ts
+++ src/data/protocols/http/index.ts
@@ -1,2 +1,14 @@
+/**
+ * Get Client
+ * */
+export * from './http-get-client';
+
+/**
+ * Post Client
+ * */
 export * from './http-post-client';
+
+/**
+ * Response
+ * */
 export * from './http-response';
