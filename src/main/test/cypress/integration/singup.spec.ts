8c64da4d026756fb8cfd2d4f3c7e03236021858f
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-27 09:58:36 -0300

    test: ensure Signup present error state if form is invalid

--- src/main/test/cypress/integration/singup.spec.ts
+++ src/main/test/cypress/integration/singup.spec.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import faker from 'faker';
+
 import * as FormHelper from '../support/form-helper';
 
 describe('Signup', () => {
@@ -17,6 +20,31 @@ describe('Signup', () => {
 
     cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly');
     FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus('name', 'O campo name tem um valor inválido');
+
+    cy.getByTestId('email').focus().type(faker.random.word());
+    FormHelper.testInputStatus('email', 'O campo email tem um valor inválido');
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus(
+      'password',
+      'O campo password tem um valor inválido'
+    );
+
+    cy.getByTestId('passwordConfirmation')
+      .focus()
+      .type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus(
+      'passwordConfirmation',
+      'O campo passwordConfirmation tem um valor inválido'
+    );
 
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
