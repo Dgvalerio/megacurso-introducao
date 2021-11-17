1e50293bf404e790c45d362a3b13cb499d29d453
dgvalerio <al.davigvalerio@gmail.com>
2021-11-16 22:23:09 -0300

    refactor: change cache storage to sync

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -2,7 +2,7 @@ import { SetStorage } from '../../data/protocols/cache/set-storage';
 
 export class LocalStorageAdapter implements SetStorage {
   // eslint-disable-next-line class-methods-use-this
-  async set(key: string, value: any): Promise<void> {
+  set(key: string, value: any): void {
     localStorage.setItem(key, value);
   }
 }
