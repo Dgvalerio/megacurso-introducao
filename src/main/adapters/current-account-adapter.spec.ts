c45f6677fc0903a4f61268e2a6f5d55e05adfcb3
dgvalerio <al.davigvalerio@gmail.com>
2021-11-17 22:42:29 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { UnexpectedError } from '../../domain/errors';
 import { mockAccountModel } from '../../domain/test';
 import { LocalStorageAdapter } from '../../infra/cache/local-storage-adapter';
 import { setCurrentAccountAdapter } from './current-account-adapter';
@@ -13,5 +14,11 @@ describe('CurrentAccountAdapter', () => {
     setCurrentAccountAdapter(account);
 
     expect(setSpy).toHaveBeenCalledWith('account', account);
+  });
+
+  test('should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined);
+    }).toThrowError(new UnexpectedError());
   });
 });
