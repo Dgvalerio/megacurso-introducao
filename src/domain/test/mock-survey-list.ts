9f1f0787dc98e749597e75fb66b30dedf1249f4e
dgvalerio <al.davigvalerio@gmail.com>
2021-11-20 15:38:58 -0300

    test: ensure SurveyItem load survey data correctly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -3,15 +3,19 @@ import * as faker from 'faker';
 
 import { SurveyModel } from '../models';
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.datatype.uuid(),
+  question: faker.random.words(10),
+  answers: [
+    { answer: faker.random.words(4), image: faker.internet.url() },
+    { answer: faker.random.words(5), image: faker.internet.url() },
+  ],
+  didAnswer: faker.datatype.boolean(),
+  date: faker.date.recent(),
+});
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.datatype.uuid(),
-    question: faker.random.words(10),
-    answers: [
-      { answer: faker.random.words(4), image: faker.internet.url() },
-      { answer: faker.random.words(5), image: faker.internet.url() },
-    ],
-    didAnswer: faker.datatype.boolean(),
-    date: faker.date.recent(),
-  },
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
 ];
