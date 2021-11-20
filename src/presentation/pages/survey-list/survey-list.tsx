9049ef0397f1f1020a7b6697fe8a91b6e58bae45
dgvalerio <al.davigvalerio@gmail.com>
2021-11-19 22:52:17 -0300

    feat: feat ensure SurveyList Component calls LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,20 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
+import { LoadSurveyList } from '../../../domain/usecases/load-survey-list';
 import { Footer, Header } from '../../components';
 import { SurveyItemEmpty } from './components';
 import Styles from './survey-list-styles.scss';
 
-const SurveyList: FC = () => (
-  <div className={Styles.surveyListWrap}>
-    <Header />
-    <div className={Styles.contentWrap}>
-      <h2>Enquetes</h2>
-      <ul data-testid="survey-list">
-        <SurveyItemEmpty />
-      </ul>
+type Props = {
+  loadSurveyList: LoadSurveyList;
+};
+
+const SurveyList: FC<Props> = ({ loadSurveyList }) => {
+  useEffect(() => {
+    (async () => {
+      await loadSurveyList.loadAll();
+    })();
+  }, []);
+
+  return (
+    <div className={Styles.surveyListWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty />
+        </ul>
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default SurveyList;
