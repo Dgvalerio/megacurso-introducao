f8f5f7617f52c3577408c9e558332e804ef773b9
dgvalerio <al.davigvalerio@gmail.com>
2021-11-28 22:31:17 -0300

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,19 +1,36 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import { ApiContext } from '../../contexts';
 import { Logo } from '../index';
 import Styles from './header-styles.scss';
 
-const Header: FC = () => (
-  <header className={Styles.headerWrap}>
-    <div className={Styles.headerContent}>
-      <Logo />
-      <div className={Styles.logoutWrap}>
-        <span>Davi</span>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">Sair</a>
+const Header: FC = () => {
+  const history = useHistory();
+  const { setCurrentAccount } = useContext(ApiContext);
+
+  const logout = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ): void => {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    history.replace('/login');
+  };
+
+  return (
+    <header className={Styles.headerWrap}>
+      <div className={Styles.headerContent}>
+        <Logo />
+        <div className={Styles.logoutWrap}>
+          <span>Davi</span>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href="#" data-testid="logout" onClick={logout}>
+            Sair
+          </a>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default memo(Header);
