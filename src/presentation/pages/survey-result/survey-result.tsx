a043750ac3b5e05c17fe8e60f2d4e0f67d3a98f3
dgvalerio <al.davigvalerio@gmail.com>
2021-12-03 00:16:21 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -6,42 +6,50 @@ import Styles from './survey-result-styles.scss';
 
 const SurveyResult: FC = () => {
   const [isLoading] = useState(false);
+  const [haveContent] = useState(false);
 
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-          <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
-              alt="React Logo"
-            />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
-              alt="React Logo"
-            />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
-              alt="React Logo"
-            />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button type="button">Voltar</button>
+        {haveContent && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>
+                Qual é seu framework web favorito? Qual é seu framework web
+                favorito? Qual é seu framework web favorito?
+              </h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+                  alt="React Logo"
+                />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+                  alt="React Logo"
+                />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+                  alt="React Logo"
+                />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button type="button">Voltar</button>
+          </>
+        )}
         {isLoading && <Loading />}
       </div>
       <Footer />
