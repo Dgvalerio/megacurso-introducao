782a3dbdef562a17d291b8ab34f5e10a090e3414
dgvalerio <al.davigvalerio@gmail.com>
2021-12-04 11:41:04 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,8 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import { screen, render, waitFor } from '@testing-library/react';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import { UnexpectedError } from '../../../domain/errors';
+import { AccessDeniedError, UnexpectedError } from '../../../domain/errors';
+import { AccountModel } from '../../../domain/models';
 import {
   LoadSurveyResultSpy,
   mockAccountModel,
@@ -13,21 +16,28 @@ import SurveyResult from './survey-result';
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy;
+  history: MemoryHistory;
+  setCurrentAccountMock(account: AccountModel): void;
 };
 
 const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  const setCurrentAccountMock = jest.fn();
+
   render(
     <ApiContext.Provider
       value={{
-        setCurrentAccount: jest.fn(),
+        setCurrentAccount: setCurrentAccountMock,
         getCurrentAccount: () => mockAccountModel(),
       }}
     >
-      <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
+      <Router history={history}>
+        <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
+      </Router>
     </ApiContext.Provider>
   );
 
-  return { loadSurveyResultSpy };
+  return { loadSurveyResultSpy, history, setCurrentAccountMock };
 };
 
 describe('SurveyResult Component', () => {
@@ -107,5 +117,20 @@ describe('SurveyResult Component', () => {
     expect(screen.queryByTestId('question')).not.toBeInTheDocument();
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
     expect(screen.getByTestId('error')).toHaveTextContent(error.message);
+  });
+
+  test('Should logout on AccessDeniedError', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
+
+    jest
+      .spyOn(loadSurveyResultSpy, 'load')
+      .mockRejectedValueOnce(new AccessDeniedError());
+
+    const { setCurrentAccountMock, history } = makeSut(loadSurveyResultSpy);
+
+    await waitFor(() => screen.getByTestId('survey-result'));
+
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
+    expect(history.location.pathname).toBe('/login');
   });
 });
