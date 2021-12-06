fb4e1573bbf18f7c013abc4cead7fc200588828c
dgvalerio <al.davigvalerio@gmail.com>
2021-12-06 12:06:24 -0300

    test: ensure SurveyResult prevent multiple answer click

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -40,11 +40,14 @@ export class LoadSurveyResultSpy implements LoadSurveyResult {
 }
 
 export class SaveSurveyResultSpy implements SaveSurveyResult {
+  callsCount = 0;
+
   params: SaveSurveyResult.Params;
 
   surveyResult = mockSurveyResultModel();
 
   async save(params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
+    this.callsCount += 1;
     this.params = params;
 
     return this.surveyResult;
